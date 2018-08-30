09401c8777a41b2be0e1413a19d6ad38684b66d1
Brendan <leungb2@student.unimelb.edu.au>
2018-08-30 15:14:18 +1000

    Google maps on app

--- app/src/main/java/com/example/sayyaf/homecare/MainActivity.java
+++ app/src/main/java/com/example/sayyaf/homecare/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.sayyaf.homecare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button mMapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMapButton = (Button) findViewById(R.id.mapButton);
+        mMapButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view == mMapButton) {
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
