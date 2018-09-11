82239754bf217e6d6c8b17e4111cf4feae48a0bf
Ckyever <ckyeverg@gmail.com>
2018-09-11 16:44:02 +1000

    Updated comments on TrackingActivity

--- app/src/main/java/com/example/sayyaf/homecare/TrackingActivity.java
+++ app/src/main/java/com/example/sayyaf/homecare/TrackingActivity.java
@@ -15,7 +15,6 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -43,49 +42,44 @@ public class TrackingActivity extends FragmentActivity implements OnMapReadyCall
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         getAllLocations();
     }
 
+    /**
+     * Iterate through all friends of the current user and calls subscribeToLocation on each one.
+     */
     private void getAllLocations() {
         // Get current user's list of assisted persons
         String path = "User/" + uid + "/friends";
 
+        // Gets reference to list of friends for current user
         DatabaseReference mRef = FirebaseDatabase.getInstance().getReference(path);
         mRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // Iterate through list of assisted persons and subscribe to updates
-                // of their location
                 for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
                     // Gets their uid to find database path to their location
                     String assistedUid = userSnapshot.getKey();
                     String locationPath = "User/" + assistedUid + "/location";
-                    subscribeToUpdates(locationPath, assistedUid);
+                    subscribeToLocation(locationPath, assistedUid);
                 }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                // Getting Post failed, log a message
                 Log.w(TAG, "Failed to get friends", databaseError.toException());
             }
         });
     }
 
-    private void subscribeToUpdates(String locationPath, String uid) {
+    /**
+     * Given a location path and a Uid, gets a snapshot of the location data for corresponding user
+     * and then passes on that information to the method getMarkerName.
+     */
+    private void subscribeToLocation(String locationPath, String uid) {
         DatabaseReference mRef = FirebaseDatabase.getInstance().getReference(locationPath);
         mRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -101,6 +95,10 @@ public class TrackingActivity extends FragmentActivity implements OnMapReadyCall
         });
     }
 
+    /**
+     * Takes a snapshot of some user's location data and their Uid, and gets the name of the
+     * corresponding user and calls setMarker.
+     */
     // Retrieves name to be used to identify the marker
     private void getMarkerName(DataSnapshot locationSnapshot, String uid) {
         String namePath = "User/" + uid + "/name";
@@ -120,13 +118,19 @@ public class TrackingActivity extends FragmentActivity implements OnMapReadyCall
         });
     }
 
+    /**
+     * Adds a new marker to the map for the given assisted person, as identified by their Uid (key).
+     * If a marker already exists then update the position.
+     */
     private void setMarker(DataSnapshot dataSnapshot, String key, String name) {
+        // Use a HashMap to hold a LatLng object for each assisted person. The key is their Uid and
+        // value is a LatLng object representing their location
         HashMap<String, Object> value = (HashMap<String, Object>) dataSnapshot.getValue();
         double lat = Double.parseDouble(value.get("latitude").toString());
         double lng = Double.parseDouble(value.get("longitude").toString());
         LatLng location = new LatLng(lat, lng);
 
-        // Icon factory for custom marker icons and ability to show multiple info windows
+        // Icon factory for custom marker icons and ability to show multiple info windows at once
         IconGenerator iconFactory = new IconGenerator(this);
 
         // New marker, so place it
@@ -141,7 +145,7 @@ public class TrackingActivity extends FragmentActivity implements OnMapReadyCall
             mMarkers.get(key).setPosition(location);
         }
 
-        // Build boundaries so we can show all markers at once
+        // Build boundaries so we can show all markers at once on upon starting the activity
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
         for (Marker marker : mMarkers.values()) {
             builder.include(marker.getPosition());
