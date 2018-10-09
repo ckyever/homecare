bc91f97b70250c867513de88c1f5968f067945a1
Sayyaf Waseem <waseems@student.unimelb.edu.au>
2018-10-09 17:37:49 +1100

    Added LoginActivity Tests

--- app/src/androidTest/java/com/example/sayyaf/homecare/TestLoginActivity.java
+++ app/src/androidTest/java/com/example/sayyaf/homecare/TestLoginActivity.java
@@ -1,35 +1,32 @@
 package com.example.sayyaf.homecare;
 
 import android.app.Activity;
-import android.support.design.widget.FloatingActionButton;
-import android.support.test.annotation.UiThreadTest;
+import android.content.ComponentName;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.support.test.InstrumentationRegistry;
-import android.os.Parcel;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Pair;
 
 import org.hamcrest.core.IsNot;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import java.util.List;
 
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
@@ -37,14 +34,17 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import com.example.sayyaf.homecare.accounts.AccountRegisterActivity;
 import com.example.sayyaf.homecare.accounts.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
+
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class TestLoginActivity {
 
     @Rule
-    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(LoginActivity.class);
+    public IntentsTestRule<LoginActivity> mActivityRule = new IntentsTestRule<>(LoginActivity.class);
     Activity activity;
     TextView mRegisterTextView;
     EditText mEmailEditText;
@@ -64,15 +64,40 @@ public class TestLoginActivity {
         mLoginButton = (Button) activity.findViewById(R.id.loginButton);
     }
 
+    @Test
+    public void checkUINotNull() {
+        assertThat(mRegisterTextView, notNullValue());
+        assertThat(mEmailEditText, notNullValue());
+        assertThat(mPasswordEditText, notNullValue());
+        assertThat(mLoginButton, notNullValue());
+
+    }
 
     @Test
-    public void checkPasswordValidity() {
+    public void checkButtonUnenabled() {
         onView(withId(R.id.emailEditText)).perform(ViewActions.clearText())
                 .perform(ViewActions.typeText("sayyaf17@hotmail.com"),closeSoftKeyboard());
         onView(withId(R.id.passwordEditText)).perform(ViewActions.clearText())
                 .perform(ViewActions.typeText(""),closeSoftKeyboard());
         onView(withId(R.id.loginButton)).perform(click());
         onView(withId(R.id.loginButton)).check(ViewAssertions.matches(IsNot.not(isEnabled())));
+    }
+
+    @Test
+    public void checkActivityChangedOnClick() {
+        onView(withId(R.id.registerTextView)).perform(click());
+        intended(hasComponent(new ComponentName(getTargetContext(), AccountRegisterActivity.class)));
+
+    }
+
+    @Test
+    public void checkButtonEnabled() {
+        onView(withId(R.id.emailEditText)).perform(ViewActions.clearText())
+                .perform(ViewActions.typeText("sayyaf17@hotmail.com"),closeSoftKeyboard());
+        onView(withId(R.id.passwordEditText)).perform(ViewActions.clearText())
+                .perform(ViewActions.typeText("friends12"),closeSoftKeyboard());
+        onView(withId(R.id.loginButton)).perform(click());
+        onView(withId(R.id.loginButton)).check(ViewAssertions.matches(isEnabled()));
     }
 
 }
