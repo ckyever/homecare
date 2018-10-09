09cf7051f5afbcad88a350c90d2f51a75e71bbd3
LazyEdward <yeungp@student.unimelb.edu.au>
2018-10-09 13:48:51 +1100

    UI elements changes for chat, contact, request page
    + menu page button for chat, contact, request page
    + change design for chat, contact, request list view block

--- app/src/main/java/com/example/sayyaf/homecare/communication/ChatActivity.java
+++ app/src/main/java/com/example/sayyaf/homecare/communication/ChatActivity.java
@@ -42,6 +42,8 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
     private Button sendMsg;
     private Button helpButton;
 
+    private Button homeButton;
+
     private ProgressBar progressBar;
     private TextView progressBarMsg;
 
@@ -61,6 +63,8 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 
         sendMsg = (Button) findViewById(R.id.sendMsg);
         helpButton = (Button) findViewById(R.id.optionHelp);
+
+        homeButton = (Button) findViewById(R.id.optionMenu);
 
         selectImage = (ImageView) findViewById(R.id.selectImage);
 
@@ -111,6 +115,10 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 
             selectImage();
             return;
+        }
+
+        if(v == homeButton){
+            chatController.returnToMenu(this);
         }
 
         if(!NetworkConnection.getConnection()){
